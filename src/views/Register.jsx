65d3d0cac4a1c0061851dd244dcbc4f164179705
Valentina Muñoz <valentinamr@gmail.com>
2023-10-28 11:46:26 -0300

    Preparando el form

--- src/views/Register.jsx
+++ src/views/Register.jsx
@@ -6,17 +6,15 @@ const Register = () => {
   const navigate = useNavigate()
   const { customer, setNewCustomer } = useContext(DataContext)
 
-  const handleBack = () => {
-    navigate(`/`)
-  }
-
   const handleSubmit = (e) => {
     e.preventDefault()
     navigate(`/`)
   }
+  const handleBack = () => {
+    navigate(`/`)
+  }
 
   const handleRegister = (e) => {
-    e.preventDefault()
     setNewCustomer([...customer, {
       firstname: firstname,
       lastname: lastname,
@@ -26,26 +24,33 @@ const Register = () => {
     navigate(`/hola`)
   }
 
+  const handleTerms = () => {
+    navigate(`/terms`)
+  }
+  const handleRecovery = () => {
+    navigate(`/recovery`)
+  }
+
   return (
     <>
       <section id="main" className="flex flex-col justify-center items-center mt-8" style={{ width: '360px' }}>
-        <div className="flex flex-row justify-start items-end w-full mb-3 p-3">
-          <div>
-            <button className="border-0 bg-transparent" onClick={handleBack}>
-              <span className="material-symbols-outlined">
-                arrow_back
-              </span>
-            </button>
-          </div>
-          <div className="text-3xl font-extrabold text-center ms-12">
-            Crea tu cuenta
-          </div>
-        </div>
-        <div className="bg-gray-300 rounded-full w-36 h-36 mb-3 text-white flex justify-center items-center">
-          <span className="material-symbols-outlined text-7xl">add_a_photo
-          </span>
-        </div>
         <form onSubmit={handleSubmit}>
+          <div className="flex flex-row justify-start items-end w-full mb-3 p-3">
+            <div>
+              <button className="border-0 bg-transparent" onClick={handleBack}>
+                <span className="material-symbols-outlined">
+                  arrow_back
+                </span>
+              </button>
+            </div>
+            <div className="text-3xl font-extrabold text-center ms-12">
+              Crea tu cuenta
+            </div>
+          </div>
+          <div id="register-photo" className="bg-gray-300 rounded-full w-36 h-36 mb-3 text-white flex justify-center items-center ms-14">
+            <span className="material-symbols-outlined text-7xl">add_a_photo
+            </span>
+          </div>
           <div id="register-inputs" className="flex flex-col items-center mb-3">
             <div className="flex flex-row mb-2">
               <div><input className="bg-gray-300 rounded-lg indent-3 p-1" type="text" id="name" placeholder="Nombre" style={{ width: '80%' }}></input></div>
@@ -68,14 +73,15 @@ const Register = () => {
               Registrarse
             </button>
             <div className="mb-2 text-2xs font-bold underline">
-            <a href="/recovery-password">Olvidé mi contraseña</a>
+              <span className="font-bold underline" onClick={handleRecovery}>Olvidé mi contraseña</span>
             </div>
           </div >
         </form>
         <div id="disclaimer" className="text-2xs text-gray-500 text-left absolute bottom-10 p-5">
-          Al registrarte, aceptas nuestras <a href="/terms"><span className="font-bold underline">Condiciones de uso</span></a> y confirmas que has leído el <a href="/terms"><span className="font-bold underline">Aviso de Privacidad</span></a> y <a href="/terms"><span className="font-bold underline">Política de Cookies</span></a>.
+          Al registrarte, aceptas nuestras <span className="font-bold underline" onClick={handleTerms}>Condiciones de uso</span> y confirmas que has leído el <span className="font-bold underline" onClick={handleTerms}>Aviso de Privacidad</span> y <span className="font-bold underline" onClick={handleTerms}>Política de Cookies</span>
         </div>
-      </section >
+
+      </section>
     </>
   )
 }
